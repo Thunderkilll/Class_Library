613130299c431b0db08d09de3400dc8c5fac59df
Khaled Guesmi <Khaled.Guesmi@mssolutions-group.com>
2022-11-28 17:07:05 +0100

    Add project files.

---
+++ LibraryTester/Program.cs
@@ -0,0 +1,106 @@
+﻿using UtilityLibraries;
+using static UtilityLibraries.StringLibrary;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+
+		#region EXPORT USER MSISDN TO EXCEL
+		//list to export
+		List<User> user_ops = new List<User>();
+		//create test values
+		User user1 = new User("54120892", "1234567891235698");
+		User user2 = new User("27950782", "1234567891235698");
+		User user3 = new User("54777321", "1234567891235698");
+		User user4 = new User("94777321", "1234567891235698");
+		//add to list
+		user_ops.Add(user1);
+		user_ops.Add(user2);
+		user_ops.Add(user3);
+		user_ops.Add(user4);
+		ExportUserInfoToExcel(user_ops);
+
+		GetColumnValue<User>(user1);
+
+		#endregion
+
+		#region Create EXCEL Operation
+
+		List<Operation> ops = new List<Operation>();
+		Operation op1 = new Operation("1", "Test1", DateTime.Now.ToString("dd-MM-HH:mm:ss:fff"), "1");
+		Operation op2 = new Operation("2", "Test2", DateTime.Now.ToString("dd-MM-HH:mm:ss:fff"), "1");
+		Operation op3 = new Operation("3", "Test3", DateTime.Now.ToString("dd-MM-HH:mm:ss:fff"), "1");
+		Operation op4 = new Operation("4", "Test4", DateTime.Now.ToString("dd-MM-HH:mm:ss:fff"), "1");
+		ops.Add(op1);
+		ops.Add(op2);
+		ops.Add(op3);
+		ops.Add(op4);
+		ExportOperationsToExcel(ops);
+		#endregion
+
+		#region FileStream
+		string path = @"C:\Users\kh.guesmi\Documents\documents\db\MyTest.txt";
+
+		// Delete the file if it exists.
+		if (File.Exists(path))
+		{
+			File.Delete(path);
+		}
+		//Create the file.
+		using (FileStream fs = File.Create(path))
+		{
+			StringLibrary.AddText(fs, "This is some text");
+			StringLibrary.AddText(fs, "This is some more text,");
+			StringLibrary.AddText(fs, "\r\nand this is on a new line");
+			StringLibrary.AddText(fs, "\r\n\r\nThe following is a subset of characters:\r\n");
+
+			for (int i = 1; i < 120; i++)
+			{
+				StringLibrary.AddText(fs, Convert.ToChar(i).ToString());
+			}
+		}
+
+		string path1 = @"C:\Users\kh.guesmi\Documents\documents\db\MyTestAsync.txt";
+		//Create file Async
+		StringLibrary.CreateFileAsync("This is my async Methode to create file ", path1);
+
+
+
+		#endregion
+
+		#region String Library
+		int row = 0;
+		do
+		{
+			if (row == 0 || row >= 25)
+				ResetConsole();
+
+			string? input = Console.ReadLine();
+
+			if (string.IsNullOrEmpty(input)) break;
+			Console.WriteLine($"Input: {input}");
+			Console.WriteLine("Begins with uppercase? " +
+				 $"{(input.StartsWithUpper() ? "Yes" : "No")}");
+			Console.WriteLine();
+			row += 4;
+		} while (true);
+		return;
+		// Declare a ResetConsole local method
+		void ResetConsole()
+		{
+			if (row > 0)
+			{
+				Console.WriteLine("Press any key to continue...");
+				Console.ReadKey();
+			}
+			Console.Clear();
+			Console.WriteLine($"{Environment.NewLine}Press <Enter> only to exit; otherwise, enter a string and press <Enter>:{Environment.NewLine}");
+			row = 3;
+		}
+		#endregion
+
+	}
+
+
+}
---
+++ StringLibrary/StringLibrary.cs
@@ -0,0 +1,149 @@
+﻿using ClosedXML.Excel;
+using System.Data;
+using System.Text;
+
+namespace UtilityLibraries;
+
+public static class StringLibrary
+{
+
+	public static bool StartsWithUpper(this string? str)
+	{
+		if (string.IsNullOrWhiteSpace(str))
+			return false;
+
+		char ch = str[0];
+		return char.IsUpper(ch);
+	}
+
+	/// <summary>
+	///  Function demonstrates some of the FileStream constructors.
+	/// </summary>
+	/// <param name="fs"></param>
+	/// <param name="value"></param>
+	public static void AddText(FileStream fs, string value)
+	{
+		byte[] info = new UTF8Encoding(true).GetBytes(value);
+		fs.Write(info, 0, info.Length);
+	}
+
+	/// <summary>
+	/// The following example shows how to write to a file asynchronously. 
+	/// The file path needs to be changed to a file that exists on the computer.
+	/// </summary>
+	/// <param name="fs"></param>
+	/// <param name="value"></param>
+	/// <param name="path"></param>
+	public static async void CreateFileAsync(string value, string path)
+	{
+		UnicodeEncoding uniencoding = new UnicodeEncoding();
+		byte[] result = uniencoding.GetBytes(value);
+		string filename = path; // string filename = @"C:\Users\kh.guesmi\Documents\documents\db\MyTestAsync.txt";
+
+
+		using (FileStream SourceStream = File.Open(filename, FileMode.OpenOrCreate))
+		{
+			SourceStream.Seek(0, SeekOrigin.End);
+			await SourceStream.WriteAsync(result, 0, result.Length);
+		}
+
+	}
+
+	/// <summary>
+	/// Create an Excel file from list of data
+	/// </summary>
+	/// <param name="ops"></param>
+	public static void ExportOperationsToExcel(List<Operation> ops)
+	{
+		DataTable dt = new DataTable("Operations");
+		dt.Columns.AddRange(new DataColumn[4] { new DataColumn("Index"),
+												new DataColumn("Description"),
+												new DataColumn("Date"),
+												new DataColumn("Result")
+		});
+
+		//fill datatable
+		foreach (var emp in ops)
+		{
+			dt.Rows.Add(emp.Index , emp.Description , emp.Date , emp.Result);
+		}
+		//using ClosedXML.Excel;
+		using (XLWorkbook wb = new XLWorkbook())
+		{
+			wb.Worksheets.Add(dt);
+			 
+		    wb.SaveAs(@"C:\Users\kh.guesmi\Documents\documents\db\ReportOperations_"+ DateTime.Now.ToString("dd_MM_HH")+".xlsx");
+		         
+		}
+	}
+
+
+	public static void ExportUserInfoToExcel(List<User> ops)
+	{
+		DataTable dt = new DataTable("Operations");
+		dt.Columns.AddRange(new DataColumn[2] { new DataColumn("MSISDN"),
+												new DataColumn("NumWallet")
+		});
+
+		//fill datatable
+		foreach (var emp in ops)
+		{
+			dt.Rows.Add(emp.Msisdn, emp.NumWallet);
+		}
+		//using ClosedXML.Excel;
+		using (XLWorkbook wb = new XLWorkbook())
+		{
+			wb.Worksheets.Add(dt);
+
+			wb.SaveAs(@"C:\Users\kh.guesmi\Documents\documents\db\ReportUserInfo  _" + DateTime.Now.ToString("dd_MM_HH") + ".xlsx");
+
+		}
+	}
+
+	public static void GetColumnValue<T>(object obj)
+	{
+		Console.WriteLine("this is"+obj.ToString());
+	}
+	public class User
+	{
+		public string Msisdn { get; set; }
+		public string NumWallet { get; set; }
+		public User(string msisdn, string numWallet)
+		{
+			Msisdn = msisdn;
+			NumWallet = numWallet;
+		}
+		public User()
+		{
+
+		}
+	}
+	public class Operation
+	{
+	 
+		public Operation()
+		{
+			Index = "0";
+			Description = "test";
+			Date = DateTime.Now.ToString("dd-MM-HH:mm:ss:fff");
+			Result = "-1";
+
+			;
+		}
+
+		public Operation(string v1, string v2, string v3, string v4)
+		{
+			this.Index = v1;
+			this.Description = v2;
+			this.Date = v3;
+			this.Result = v4;
+		}
+
+		public string Index { get; set; }
+		public string Description { get; set; }
+		public string Date { get; set; }
+		public string Result { get; set; }
+	}
+
+
+}
